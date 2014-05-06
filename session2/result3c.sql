a64c5fe04240b2e47dbb86ef26038271eeeeb9ba
DennisdeW <dennisdw@outlook.com>
2014-05-06 15:49:53 +0200

    3c -2

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,3 +1,8 @@
 SELECT p.pid
 FROM Person p
-WHERE 'de persoon in een regisseurloze film gespeeld heeft';
+WHERE EXISTS (
+	SELECT *
+	FROM Acts a
+	WHERE p.pid = a.pid
+	AND NOT EXISTS 'Een regisseur met dezelfde mid'
+	);
