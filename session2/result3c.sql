59fdc0de16ba0dacda7ba9592e94c7fa676c98c0
DennisdeW <dennisdw@outlook.com>
2014-05-06 15:52:26 +0200

    Revert "3c -2"
    This reverts commit a64c5fe04240b2e47dbb86ef26038271eeeeb9ba.

--- session2/result3c.sql
+++ session2/result3c.sql
@@ -1,8 +1,3 @@
 SELECT p.pid
 FROM Person p
-WHERE EXISTS (
-	SELECT *
-	FROM Acts a
-	WHERE p.pid = a.pid
-	AND NOT EXISTS 'Een regisseur met dezelfde mid'
-	);
+WHERE 'de persoon in een regisseurloze film gespeeld heeft';
