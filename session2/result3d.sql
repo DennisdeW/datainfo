367ad2db45a4703085834c4249c266545d5dfe0a
DennisdeW <dennisdw@outlook.com>
2014-05-06 16:09:30 +0200

    3d - 6
    Shunting op binnnenste subquery

--- session2/result3d.sql
+++ session2/result3d.sql
@@ -6,12 +6,8 @@ WHERE EXISTS (
 	WHERE p.pid = w.pid
 	AND NOT EXISTS (
 		SELECT *
-		FROM Directs d
-		WHERE EXISTS (
-			SELECT *
-			FROM Writes w2
-			WHERE w.pid = w2.pid
-			AND d.mid = w2.mid
-		)
+		FROM Directs d, Writes w2
+		WHERE w.pid = w2.pid
+		AND d.mid = w2.mid
 	)
 );
